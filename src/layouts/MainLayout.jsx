46b60ed099161f33be26559a1fb941b18bca56bd
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-09-22 04:10:21 +0600

    loc added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Outlet, useLoaderData, useNavigation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLoaderData, useLocation, useNavigation } from 'react-router-dom';
 import Navbar from '../components/Header/Navbar/Navbar';
 
 const MainLayout = () => {
@@ -9,6 +9,30 @@ const MainLayout = () => {
     const isLoadingState = navigation.state === 'loading'
 
     console.log(isLoadingState);
+
+
+    const loc = useLocation()
+
+    useEffect(()=>{
+
+        console.log("useEffect loc",loc);
+    
+        if (loc.pathname === '/') {
+          document.title = `Phone - home`
+        }
+        else{
+          document.title = `Phone${loc.pathname.replace('/','-')}`
+        }
+    
+        if (loc.state) {
+          document.title = loc.state
+        }
+    
+    
+      },[loc.pathname,loc])
+    
+      console.log(loc);
+
 
     return (
         <div className='max-w-[1600px] mx-auto'>
